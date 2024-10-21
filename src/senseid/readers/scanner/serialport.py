4a0976ea3386ae4bc597b06b216bc5aef45d1f39
iz2k <ibon@zalbide.com>
2024-10-21 13:36:13 +0200

    scan serial port by PID instead of name

--- src/senseid/readers/scanner/serialport.py
+++ src/senseid/readers/scanner/serialport.py
@@ -35,14 +35,16 @@ class SerialPortScanner:
             com_port_list = serial.tools.list_ports.comports()
             for com_port in com_port_list:
                 # REDRCP
-                if 'Silicon Lab' in str(com_port.manufacturer):
+                #if 'Silicon Lab' in str(com_port.manufacturer):
+                if 'VID:PID=10C4:EA60' in str(com_port.hwid):
                     if com_port.name not in self.comports:
                         logger.info('New REDRCP reader found: ' + com_port.name)
                         self.comports.append(com_port.name)
                         self.notification_callback(SenseidReaderConnectionInfo(driver=SupportedSenseidReader.REDRCP,
                                                                                connection_string=com_port.name))
                 # NUR
-                if 'NUR Module' in str(com_port.manufacturer):
+                #if 'NUR Module' in str(com_port.manufacturer):
+                if 'VID:PID=04E6:0112' in str(com_port.hwid):
                     if com_port.name not in self.comports:
                         logger.info('New NUR reader found: ' + com_port.name)
                         self.comports.append(com_port.name)
